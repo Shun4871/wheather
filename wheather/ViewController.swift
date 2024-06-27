89b74a6a718bbcdde6411d05c7c5c2f6bb7b119c
ShunnosukeTsuge <shuntsugework@gmail.com>
2024-06-28 01:38:35 +0900

    修正

--- wheather/ViewController.swift
+++ wheather/ViewController.swift
@@ -1,17 +1,28 @@
-//
-//  ViewController.swift
-//  wheather
-//
-//  Created by 柘植俊之介 on 2024/05/27.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-    
-    // TabBarの背景色を保存するためのUserdDefaults
+
+    // TabBarの背景色を保存するためのUserDefaults
     var tabBarColorData: UserDefaults = UserDefaults.standard
+
+    // Apple Weatherの商標と法的ソースリンクを表示するためのラベル
+    let attributionLabel: UILabel = {
+        let label = UILabel()
+        label.text = " Weather"
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
     
+    let attributionLinkLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Apple Weather Legal Attribution"
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.textColor = .blue
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,6 +33,7 @@ class ViewController: UIViewController {
             changeTabBarColor(barColor)
         }
         
+        setupAttributionLabels()
     }
     
     /**
@@ -72,6 +84,37 @@ class ViewController: UIViewController {
             // UserDefaultsにタブバーの背景色を文字列で保存する
             tabBarColorData.set(colorString, forKey: "tabBarColor")
             
+        }
+    }
+
+    /**
+     Apple Weatherの商標と法的ソースリンクを設定する関数
+     */
+    func setupAttributionLabels() {
+        view.addSubview(attributionLabel)
+        view.addSubview(attributionLinkLabel)
+        
+        // Apple Weather商標のラベルの制約を設定
+        NSLayoutConstraint.activate([
+            attributionLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            attributionLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -40)
+        ])
+        
+        // 法的ソースリンクのラベルにタップジェスチャーを追加
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(openLegalAttribution))
+        attributionLinkLabel.isUserInteractionEnabled = true
+        attributionLinkLabel.addGestureRecognizer(tapGesture)
+        
+        // 法的ソースリンクのラベルの制約を設定
+        NSLayoutConstraint.activate([
+            attributionLinkLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            attributionLinkLabel.topAnchor.constraint(equalTo: attributionLabel.bottomAnchor, constant: 8)
+        ])
+    }
+    
+    @objc func openLegalAttribution() {
+        if let url = URL(string: "https://weatherkit.apple.com/legal-attribution.html") {
+            UIApplication.shared.open(url)
         }
     }
 }
